fbbdb6f8a137b5b434505e976a878b9011342b0a
Ryan Chern <ryan.t.chern@gail.com>
2017-10-25 17:18:37 -0700

    everything working but high score

--- Lab3Clean/app.js
+++ Lab3Clean/app.js
@@ -1,12 +1,37 @@
 var app = require("express")();
 var handler = require("./userHandler");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var topTen = new Array(10);
 
 let path = require("path");
 
 
 app.get("/", function(req, res) {
+    console.log("server successfully started");
     res.sendFile(path.resolve("RacerType.html"));
 });
+
+app.post("/server", function(req, res) {
+    console.log(req.body);
+
+    
+    topTen.push(req.body);
+
+    console.log(topTen);
+
+    res.status(200);
+    res.end();
+})
+
+app.get("/server", function(req, res) {
+    console.log("server up");
+    res.sendFile(path.resolve("highscores.json"));
+})
+
 
 app.listen(3000);
 console.log("Listening");
