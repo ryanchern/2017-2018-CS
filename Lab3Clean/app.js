62d62062f2127759eed71500438de78d669c6492
Ryan Chern <ryan.t.chern@gail.com>
2017-11-01 09:23:17 -0700

    high score list working

--- Lab3Clean/app.js
+++ Lab3Clean/app.js
@@ -3,32 +3,58 @@ var handler = require("./userHandler");
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var topTen = new Array(10);
+var topScores = new Array();
 
 let path = require("path");
 
+var fs = require("fs");
 
-app.get("/", function(req, res) {
+var initial = 0;
+
+app.get("/", function (req, res) {
     console.log("server successfully started");
     res.sendFile(path.resolve("RacerType.html"));
+    if (initial == 0) {
+        fs.writeFile("highscores.json", "", function (err) {
+
+        });
+        initial = initial + 1;
+    }
 });
 
-app.post("/server", function(req, res) {
-    console.log(req.body);
+app.post("/server", function (req, res) {
+    //console.log(req.body);
+    if (topScores.length == 0) {
+        topScores.push(req.body);
+    }
+    else {
+        for (i=0; i<topScores.length; i++) {
+            //console.log(topScores[i][1]);
+            if (req.body[1] >= topScores[i][1]) {
+                console.log("HERE");
+                topScores.splice(i, 0, req.body);
+                break;
+            }
+            else {
 
+            }
+        }
+        
+    }
+
+    for (i=0; i<topScores.length; i++) {
+        fs.writeFile("highscores.json", topScores[i], function (err) {
+        }); <br/> <n/>
+    }
     
-    topTen.push(req.body);
-
-    console.log(topTen);
 
     res.status(200);
     res.end();
 })
 
-app.get("/server", function(req, res) {
-    console.log("server up");
+app.get("/highscores", function (req, res) {
     res.sendFile(path.resolve("highscores.json"));
 })
 
