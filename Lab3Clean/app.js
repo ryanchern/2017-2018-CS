84d841a5748a359693a54b8c83a4474884f41bfd
Ryan Chern <ryan.t.chern@gail.com>
2017-11-02 14:49:16 -0700

    working on high score list

--- Lab3Clean/app.js
+++ Lab3Clean/app.js
@@ -16,16 +16,18 @@ var initial = 0;
 app.get("/", function (req, res) {
     console.log("server successfully started");
     res.sendFile(path.resolve("RacerType.html"));
+    /*
     if (initial == 0) {
-        fs.writeFile("highscores.json", "", function (err) {
+        fs.writeFile("highscores.html", "", function (err) {
 
         });
         initial = initial + 1;
     }
+    */
 });
 
 app.post("/server", function (req, res) {
-    //console.log(req.body);
+    console.log(req.body);
     if (topScores.length == 0) {
         topScores.push(req.body);
     }
@@ -42,20 +44,24 @@ app.post("/server", function (req, res) {
             }
         }
         
+        for (i=0; i<topScores.length; i++) {
+            console.log(topScores[i]);
+        }
     }
 
-    for (i=0; i<topScores.length; i++) {
-        fs.writeFile("highscores.json", topScores[i], function (err) {
-        }); <br/> <n/>
-    }
+
+    //fs.writeFile("highscores.html", "<html> <head> <title> Highscores </title> </style>  <style> body { margin: 0; padding: 0;} div#scorelist {text-align: center; height: 100%; background-color: blue;} </style> </head> <body> <div id = "scorelist"> Test </div> </body> </html>", function (err) {
+    //});
     
+    fs.writeFile("highscores.html", "<html> <head> <style> body {margin: 0; padding: 0; text-align: center;} div#container {background-color: red; height: 100%; width 50%;} </style> </head> <body> <div id='container'> test </div> </body> </html>", function(err) {
+    });
 
     res.status(200);
     res.end();
 })
 
 app.get("/highscores", function (req, res) {
-    res.sendFile(path.resolve("highscores.json"));
+    res.sendFile(path.resolve("highscores.html"));
 })
 
 
